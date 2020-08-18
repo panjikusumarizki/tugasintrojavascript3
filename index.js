5ba5d7b5714f15deb664339557692b710e498d73
yyy-pixel <pnksm009@gmail.com>
2020-08-18 08:06:42 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,41 @@ const fetch = require('node-fetch')
 
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(json => json.map(a => console.log(a.name)))
+    .then(json => {json.map(a => console.log(a.name))})
     .catch(error => {
         console.log(error.message)
     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/', (request, response) => {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(json => response.send(json.map(a => a.name)))
+//     .catch(error => {
+//         console.log(error.message)
+//     })
+// })
+
+// app.listen(3000, () => {
+//     console.log('App running on http://localhost:3000')
+// })
